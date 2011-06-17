e3a071b569c108a109e0c2bdbf3a2bf556b98716
Jim <countingtoten@gmail.com>
2011-06-16 22:41:09 -0500

    Add API key header file for anyone using the open source project

---
+++ Classes/StudyDictionaryAPIConstants.h
@@ -0,0 +1,10 @@
+//
+//  StudyDictionaryAPIConstants.h
+//  StudyDictionary
+//
+//  Created by James Weinert on 4/8/11.
+//  Copyright 2011 Weinert Works. All rights reserved.
+//
+
+#define ADMOB_API_KEY   @"YOUR ADMOB API KEY GOES HERE"
+#define WORDNIK_API_KEY @"YOUR WORKNIK API KEY GOES HERE"
